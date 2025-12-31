35182159f0d079b8dfadd67a595f4522fa7d6a01
TamerTimraz <tamertimraz04@gmail.com>
2025-12-31 16:35:40 -0500

    configured supabase and auth

---
+++ proxy.ts
@@ -0,0 +1,40 @@
+import { NextResponse, type NextRequest } from 'next/server'
+import { createServerClient } from '@supabase/ssr'
+
+export async function proxy(request: NextRequest) {
+  let response = NextResponse.next({
+    request,
+  })
+
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll()
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value }) => {
+            request.cookies.set(name, value)
+          })
+
+          response = NextResponse.next({
+            request,
+          })
+
+          cookiesToSet.forEach(({ name, value, options }) => {
+            response.cookies.set(name, value, options)
+          })
+        },
+      },
+    }
+  )
+
+  // IMPORTANT:
+  // This keeps the Supabase session in sync between server and browser.
+  // Do not run any code between client creation and this call.
+  await supabase.auth.getUser() // refreshes the auth session
+
+  return response
+}
